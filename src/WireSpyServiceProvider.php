482ecff8e96fde7f831a0258e6d7671470d1742e
Philo Hermans <me@philo.dev>
2024-10-09 15:42:16 +0200

    Cleanup service provider

--- src/WireSpyServiceProvider.php
+++ src/WireSpyServiceProvider.php
@@ -10,13 +10,30 @@ class WireSpyServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        Livewire::component('wire-spy', WireSpy::class);
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'wire-spy');
+        $this->registerLivewireComponent();
+        $this->registerBladeViews();
+        $this->registerLivewireComponentHooks();
+        $this->registerHotReloadRoute();
+    }
 
-        app('livewire')->componentHook(SupportProfilling::class);
+    private function registerLivewireComponent(): void
+    {
+        Livewire::component('wire-spy', WireSpy::class);
+    }
+
+    private function registerBladeViews(): void
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'wire-spy');
+    }
+
+    private function registerLivewireComponentHooks(): void
+    {
         app('livewire')->componentHook(SupportHotReloading::class);
         app('livewire')->componentHook(SupportAutoInjectedAssets::class);
+    }
 
+    private function registerHotReloadRoute(): void
+    {
         Route::get('/wire-spy/hot-reload', [SupportHotReloading::class, 'route']);
     }
 }
