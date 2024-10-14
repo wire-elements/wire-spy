9782241d0691826f81452ff5bf9b04178f3e8fb9
Philo Hermans <me@philohermans.com>
2024-10-14 23:39:19 +0200

    Update wire-spy.php

--- src/config/wire-spy.php
+++ src/config/wire-spy.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    /*
+     * Enable or disable WireSpy.
+     */
+    'enabled' => env('WIRESPY_ENABLED', true),
+    
     /**
     * The keybinding configuration option allows you to define a keyboard shortcut
     * using AlpineJS syntax. It accepts a string representing the desired key combination.
@@ -10,6 +15,4 @@ return [
     * - Combine with other keys using dot notation, like 'super.l' for 'Cmd+L' or 'Ctrl+L'.
     */
     'keybinding' => 'super.l',
-
-    'enabled' => env('WIRESPY_ENABLED', true),
 ];
